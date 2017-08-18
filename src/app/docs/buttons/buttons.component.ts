ecac8ec74ae85c31d8a812777e71b886b798d510
Vinay <Vinay.bv@accionlabs.com>
2017-08-18 19:47:53 +0530

    chmages

--- src/app/docs/buttons/buttons.component.ts
+++ src/app/docs/buttons/buttons.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-
+  public tab:any = "example";
   constructor() { }
 
   ngOnInit() {
