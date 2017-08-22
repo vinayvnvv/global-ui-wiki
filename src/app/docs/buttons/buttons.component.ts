893cbf02476dff930c28c073ae3ebef7a0466bdf
Vinay <Vinay.bv@accionlabs.com>
2017-08-22 20:05:12 +0530

    changes

--- src/app/docs/buttons/buttons.component.ts
+++ src/app/docs/buttons/buttons.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare const Prism: any;
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.css']
 })
-export class ButtonsComponent implements OnInit {
+export class ButtonsComponent implements OnInit, AfterViewInit {
   public tab:any = "example";
   constructor() { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+  	this.highlightSyntax();
+  }
+
+
+  highlightSyntax() {
+  	 if(window["Prism"])
+  		Prism.highlightAll();
   }
 
 }
