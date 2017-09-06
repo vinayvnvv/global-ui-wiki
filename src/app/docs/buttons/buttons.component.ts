3ab6811981a016821d602e748c57d40320d202a3
Vinay <Vinay.bv@accionlabs.com>
2017-09-06 20:17:05 +0530

    changes

--- src/app/docs/buttons/buttons.component.ts
+++ src/app/docs/buttons/buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, HostListener } from '@angular/core';
 
 declare const Prism: any;
 
@@ -15,15 +15,33 @@ export class ButtonsComponent implements OnInit, AfterViewInit {
   }
   ngAfterViewInit() {
   	this.highlightSyntax();
+    this.placeTOC();
   }
 
 
   highlightSyntax() {
   	 if(window["Prism"])
   		Prism.highlightAll();
-
-     var h = window.innerHeight -  document.getElementById("toc-container").offsetTop;
-     document.getElementById("toc-container").style.height = (h-70) + "px";
   }
+
+       
+
+  placeTOC() {
+     let e = document.getElementById("toc-container");
+     let h = window.innerHeight -  e.offsetTop;
+     e.style.height = (h-70) + "px";
+     let scroller = document.getElementById("nav-content");
+     scroller.addEventListener('scroll', (ev)=> {
+        if(ev.target["id"] != "nav-content") return;
+        let scrolled = ev.target["scrollTop"];
+        console.log(ev.target["scrollTop"], ev)
+        if(scrolled < 184 ) {
+          e.style.marginTop = "-" + (scrolled+95) + "px";
+        }
+    }, true);
+    }
+     
+
+  
 
 }
